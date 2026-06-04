e22206d8ba30e83b923eeb00ccd6c7c76a5b6d89
deepthi-Keenminds <deepthi.prasanna@keenminds.in>
2026-06-04 22:12:53 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,91 @@
+// Task-1: Variable Practice
+
+const studentName = "Naveen";
+const studentAge = 18;
+const studentCourse = "MERN Stack";
+
+console.log(studentName);
+console.log(studentAge);
+console.log(studentCourse);
+
+
+// Task-2: Var, Let, Const
+
+// Create variables
+var company = "Stackly";
+let salary = 20000;
+const country = "India";
+
+// Update values
+company = "Google";
+salary = 30000;
+
+// Print all values
+
+console.log("Company:", company);
+console.log("Salary:", salary);
+console.log("Country:", country);
+
+// Task-3: User Name
+
+let name = prompt("What is your name?");
+
+console.log("Welcome " + name);
+
+// Task 4: Confirm Box
+
+let result = confirm("Do you know JavaScript?");
+
+console.log(result);
+
+// Task 5:  Student Information
+
+// let studentName = prompt("Enter Student Name:");
+// let studentAge = prompt("Enter Student Age:");
+// let studentCity = prompt("Enter Student City:");
+
+// console.log("Name : " + studentName);
+// console.log("Age : " + studentAge);
+// console.log("City : " + studentCity);
+
+// Task 6: Alert Message
+
+let course = "JavaScript";
+
+alert("Welcome to " + course);
+
+// Task 7: Document Write
+
+document.writeln("Hello Student");
+document.writeln("<br>");
+document.writeln("Welcome to JavaScript");
+
+// Task 8: Favorite Things
+
+let favoriteFood = "Chicken Biryani";
+let favoriteColor = "Black";
+let favoriteMovie = "Leo";
+
+console.log("Food : " + favoriteFood);
+console.log("Color : " + favoriteColor);
+console.log("Movie : " + favoriteMovie);
+
+// Task 9: Mini Bio Data
+
+// let name = "Naveen";
+// let age = 18;
+// let phone = "9876543210";
+// let email = "naveen@gmail.com";
+
+// console.log("Name : " + name);
+// console.log("Age : " + age);
+// console.log("Phone : " + phone);
+// console.log("Email : " + email);
+
+
+// Task 10: (Challenge)
+
+// let name = prompt("Enter Your Name:");
+// let course = prompt("Enter Your Course:");
+
+// alert("Hello " + name + ", Welcome to " + course + " Course");
